0ed16d564e1f3e7d4e1041591e444a461a9e27db
Felipe Matheus <felipe_cavalcante7939@hotmail.com>
2021-09-28 17:47:41 -0300

    Adding atividade 3A02 and 3A03

--- learning-redux/src/pages/fruitsPage/index.jsx
+++ learning-redux/src/pages/fruitsPage/index.jsx
@@ -1,13 +1,31 @@
-import { useSelector } from "react-redux";
 import "./style.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addFruit } from "../../store/modules/fruits/actions";
 
 const FruitsPage = () => {
+  const [fruit, setFruit] = useState("");
+  const dispatch = useDispatch();
   const fruits = useSelector((state) => state.fruits);
 
+  const handleAddFruit = () => dispatch(addFruit(fruit));
+
   return (
-    <div className={"fruitDiv"}>
+    <div className={"container"}>
+      <div>
+        <input
+          className={"input"}
+          onChange={(event) => setFruit(event.target.value)}
+        />
+        <button className={"button"} onClick={handleAddFruit}>
+          Adicionar
+        </button>
+      </div>
+
       {fruits.map((fruit) => (
-        <p className={"fruit"} props={fruit} key={fruit}> {fruit} </p>
+        <p className={"fruit"} key={fruit}>
+          {fruit}
+        </p>
       ))}
     </div>
   );
